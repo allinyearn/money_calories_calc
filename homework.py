b509f06685dc423ba307765b4e22282a1685e7bc
Александр <ruyearn@gmail.com>
2021-03-21 13:40:50 +0300

    fixed pep

--- homework.py
+++ homework.py
@@ -85,23 +85,3 @@ class CaloriesCalculator(Calculator):
             )
         else:
             return 'Хватит есть!'
-
-
-# создадим калькулятор денег с дневным лимитом 1000
-cash_calculator = CashCalculator(1000)
-
-# дата в параметрах не указана,
-# так что по умолчанию к записи
-# должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000,
-                                  comment='бар в Танин др',
-                                  date='08.11.2019'))
-
-print(cash_calculator.get_today_cash_remained('eur'))
-# должно напечататься
-# На сегодня осталось 555 руб
-print(cash_calculator.get_week_stats)
